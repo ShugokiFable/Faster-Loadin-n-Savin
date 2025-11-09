fa2d2d2c236d7b37b1b7b9c3a265585b63fe4c09
Harry Gillanders <contact@harrygillanders.com>
2025-11-09 21:43:34 +0000

    Update the version-number to v1.0.7.

--- mod/slack_common/user_interface.d
+++ mod/slack_common/user_interface.d
@@ -8,7 +8,7 @@ import slack_common.memory;
 import slack_common.text;
 
 
-enum immutable(wchar[]) errorDialogTitle = "Save & Load Accelerator for SKSE Cosaves v1.0.6 Error";
+enum immutable(wchar[]) errorDialogTitle = "Save & Load Accelerator for SKSE Cosaves v1.0.7 Error";
 
 
 void reportErrorToUser (scope const(wchar)* message, uint flags = MB_ICONERROR) nothrow @nogc
